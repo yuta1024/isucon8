54863b96e5bcbf07a7c6a0d34166869b5ae2553b
Yuta Harima <yuu725@gmail.com>
2018-09-16 14:45:53 +0900

    deploy script is safeeeeee

--- deploy-db.sh
+++ deploy-db.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if [ `hostname` != '118-27-6-207' ]; then
+  echo "abort: This server is not DB server!"
+  exit 1
+fi
+
 # nginx
 sudo cp ./infra/nginx/nginx-lb.conf /etc/nginx/nginx.conf
 
