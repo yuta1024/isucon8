31c00a7e7e66c95e1743f70051bc9f024c143992
Yuta Harima <yuu725@gmail.com>
2018-09-16 14:26:36 +0900

    separate deploy script

--- db/init-user.sh
+++ db/init-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <<'EOF' | mysql -uroot
+cat <<'EOF' | mysql -h172.17.119.2 -uroot -p'knishiya'
 CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
 GRANT ALL ON torb.* TO 'isucon'@'%';
 CREATE USER 'isucon'@'localhost' IDENTIFIED BY 'isucon';
--- db/init.sh
+++ db/init.sh
@@ -6,8 +6,8 @@ BENCH_DIR="$ROOT_DIR/bench"
 
 export MYSQL_PWD=isucon
 
-mysql -uisucon -e "DROP DATABASE IF EXISTS torb; CREATE DATABASE torb;"
-mysql -uisucon torb < "$DB_DIR/schema.sql"
+mysql -h172.17.119.2 -uisucon -e "DROP DATABASE IF EXISTS torb; CREATE DATABASE torb;"
+mysql -h172.17.119.2 -uisucon torb < "$DB_DIR/schema.sql"
 
 if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
   echo "Run the following command beforehand." 1>&2
@@ -15,6 +15,6 @@ if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
   exit 1
 fi
 
-mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
-gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
-mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+mysql -h172.17.119.2 -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
+gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -h172.17.119.2 -uisucon torb
+mysql -h172.17.119.2 -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
